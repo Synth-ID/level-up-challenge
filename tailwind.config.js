ad2ded5bad1fef4bc961d79eae5c78b47db9b0d0
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-10-09 19:25:34 +0700

    Set default theme for landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        landing: ['Open Sans', 'sans-serif'],
+        landingCourses: ['Poppins', 'sans-serif'],
+        landingIntructors: ['Inter', 'sans-serif']
+      },
+      colors: {
+        landingPrimary: '#0F756B',
+        landingSecondary: '#3B7586',
+        landingTertiary: '#1B353D',
+      }
+    },
   },
   plugins: [],
 }
