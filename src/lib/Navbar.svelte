3ceb8689f26aebcf0742231d6cbeeee59724de2a
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-10-10 09:38:57 +0700

    Prepare for header

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,9 @@
+<nav>
+
+</nav>
+
+<style lang="postcss">
+    nav {
+        @apply sticky top-0 w-full bg-white text-gray-300 py-8 z-50
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,13 @@
     import "@fontsource-variable/open-sans";
     import "@fontsource/poppins";
     import "@fontsource-variable/inter";
+
+    import Navbar from "$lib/Navbar.svelte"
 </script>
+
+<header>
+    <Navbar />
+</header>
 
 <slot />
 
