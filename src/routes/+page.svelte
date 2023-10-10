b1a7bc33620dbb690e3c4d2c773f5257231a0a58
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-10-10 13:35:55 +0700

    Implement CTA

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
     <div class="max-w-screen-xl mx-auto">
         <div class="flex items-center space-x-24">
             <div class="flex flex-col space-y-8 w-1/2">
-                <h1 class="text-6xl font-bold font-landingIntructors">Take your career to the next level.</h1>
+                <h2 class="text-6xl font-bold font-landingIntructors">Take your career to the next level.</h2>
                 <p class="text-3xl font-medium">With indispensable courses</p>
                 <div class="space-x-8 font-semibold">
                     <button class="px-6 py-3 rounded-md border">Exams</button>
@@ -23,6 +23,39 @@
             <button class="w-12 h-12 backdrop-brightness-75 backdrop-blur rounded-full">
                 <Icon icon="mdi:chevron-right" class="w-full" />
             </button>
+        </div>
+    </div>
+</section>
+
+<!-- CTA -->
+<section class="py-32 text-gray-900">
+    <div class="max-w-screen-xl mx-auto space-y-24">
+        <div class="text-center">
+            <h1 class="text-6xl font-bold">Enrol for a couse now</h1>
+        </div>
+        <div class="flex items-center space-x-24">
+            <div class="flex flex-col space-y-8 w-1/2">
+                <h2 class="text-6xl font-bold">Take your career to the next level.</h2>
+                <p class="text-3xl">With indispensable courses</p>
+                <p class="text-lg">Lorem ipsum dolor sit at, consectetur adipielit. Facilisi fermentum, dignissim pharetra. Aliquam Lorem ipsum dolor sit at, consectetur adipielit. Facilisi fermentum, dignissim pharetra. Aliquam Lorem ipsum dolor sit at, consectetur adipielit. Facilisi fermentum, dignissim pharetra.</p>
+            </div>
+            <form class="flex flex-col space-y-8 w-1/2 bg-white p-12 shadow-xl rounded-lg">
+                <h3 class="font-medium text-xl">Registration for enrolment</h3>
+                <span></span>
+                <input class="border border-gray-200 bg-gray-50 h-12 px-5 rounded-md focus:outline-none"
+                    type="text" name="name" placeholder="Full name">
+                <input class="border border-gray-200 bg-gray-50 h-12 px-5 rounded-md focus:outline-none"
+                    type="email" name="email" placeholder="Email">
+                <input class="border border-gray-200 bg-gray-50 h-12 px-5 rounded-md focus:outline-none"
+                    type="tel" name="phone" placeholder="Phone number">
+                <button class="border border-gray-200 bg-gray-50 h-12 px-5 rounded-md focus:outline-none flex items-center">
+                    <Icon icon="mdi:snowflake" class="text-2xl mr-4" />
+                    <span>List of courses goes here</span>
+                    <Icon icon="mdi:chevron-down" class="text-2xl ml-auto"/>
+                </button>
+                <span></span>
+                <input class="px-6 py-3 rounded-md bg-landingTertiary text-white w-1/2 cursor-pointer ml-auto" type="submit" value="Apply Now" />
+            </form>
         </div>
     </div>
 </section>
